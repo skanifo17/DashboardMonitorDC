8012c1b1e8da28c6bf3d9f7df73b34a296e66641
skanifo17 <ekopujiyanto117@gmail.com>
2026-01-23 00:47:48 +0700

    Update gsheet.py

--- gsheet.py
+++ gsheet.py
@@ -4,19 +4,28 @@ import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
 
 def load_sheet(sheet_name):
-    scope = [
-        "https://spreadsheets.google.com/feeds",
-        "https://www.googleapis.com/auth/drive"
-    ]
+    try:
+        scope = [
+            "https://www.googleapis.com/auth/spreadsheets.readonly",
+            "https://www.googleapis.com/auth/drive.readonly"
+        ]
 
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(
-        st.secrets["google_service_account"],
-        scope
-    )
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(
+            st.secrets["google_service_account"],
+            scope
+        )
 
-    client = gspread.authorize(creds)
-    ws = client.open_by_key(
-        st.secrets["general"]["GSHEET_KEY"]
-    ).worksheet(sheet_name)
+        client = gspread.authorize(creds)
 
-    return pd.DataFrame(ws.get_all_records())
+        sheet_id = st.secrets["general"]["GSHEET_KEY"]
+
+        sh = client.open_by_key(sheet_id)
+
+        ws = sh.worksheet(sheet_name)
+
+        return pd.DataFrame(ws.get_all_records())
+
+    except Exception as e:
+        st.error(f"❌ Gagal load sheet '{sheet_name}'")
+        st.error(str(e))
+        st.stop()
