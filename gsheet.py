7bc41691b9b512114d685a636209df23fd7a8f30
skanifo17 <ekopujiyanto117@gmail.com>
2026-01-23 00:08:13 +0700

    Update gsheet.py

--- gsheet.py
+++ gsheet.py
@@ -1,16 +1,22 @@
+import streamlit as st
 import gspread
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
-from config import GSHEET_KEY
 
 def load_sheet(sheet_name):
     scope = [
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive"
     ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        "service_account.json", scope
+
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(
+        st.secrets["google_service_account"],
+        scope
     )
+
     client = gspread.authorize(creds)
-    ws = client.open_by_key(GSHEET_KEY).worksheet(sheet_name)
+    ws = client.open_by_key(
+        st.secrets["general"]["GSHEET_KEY"]
+    ).worksheet(sheet_name)
+
     return pd.DataFrame(ws.get_all_records())
