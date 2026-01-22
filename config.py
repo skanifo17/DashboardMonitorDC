b52ad4055e6bf1a37a708ede45fed74d5409553e
skanifo17 <ekopujiyanto117@gmail.com>
2026-01-23 00:01:11 +0700

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-GSHEET_KEY = "1nevirGkRG17kJAkKCczSDe9TN51tR3KQMZSJnmBML3s"
-
-LEAD_TIME = 5
-DOC_ALERT = 3
-UTIL_ALERT = 90
-
-WA_API_URL = "https://api.callmebot.com/whatsapp.php"
-WA_GROUP_ID = "ISI_GROUP_ID"
-WA_API_KEY = "ISI_API_KEY"
